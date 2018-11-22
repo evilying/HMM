794a262a1b7cd3660a3dc060ac554abfb7f1a507
evilying <evilying2016@gmail.com>
2018-11-21 20:54:20 -0500

    [M] test N-gram

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import numpy as np
-from utility import add2dict, list2pdf, sample_word
+from utility import add2dict, list2pdf, sample_word, remove_punctuation
+import string, unicodedata
 
-wlist = ['cat', 'cat', 'dog']
+data_dir = './data/'
 
-dict_pdf = list2pdf(wlist)
+initial = {}
+second_word = {}
+transitions = {}
 
-print(dict_pdf)
+filename = data_dir + 'robert_frost.txt'
+contents = open(filename, 'r', encoding = 'utf-8')
 
-word_initial = sample_word(dict_pdf, 0)
+line = contents.readline()
+print(line)
+tokens = remove_punctuation(line.rstrip().lower()).split()
+n_gram = 1
+word_grams = {}
+for i in range(len(tokens)-n_gram):
 
-print(word_initial)
+    gram = ' '.join(tokens[i: i + n_gram])
+    if gram not in word_grams.keys():
+        word_grams[gram] = []
+    word_grams[gram].append(tokens[i + n_gram])
+print(word_grams)
